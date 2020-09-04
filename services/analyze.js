d133abfe0e2aee26f2340ca64f4b8ed73a67c6b7
Erik <46797334+ErikOverflow@users.noreply.github.com>
2020-09-03 19:42:12 -0500

    Add time ticks

--- services/analyze.js
+++ services/analyze.js
@@ -3,25 +3,32 @@ const { matchDetail } = require("./matches");
 const breakdown = async (req, res) => {
   const gameId = req.query.gameId;
   const region = req.query.region;
-  let timelineDoc = await timelineByGameId(gameId, region);
   let detailsDoc = await matchDetail(gameId, region);
+
+  //Add participants and their stat history
   const participants = {};
-  const participantStatHistory = {};
   for (const participantIdentity of detailsDoc.participantIdentities){
     participants[participantIdentity.participantId] = {
       summonerName: participantIdentity.player.summonerName,
-      accountId: participantIdentity.player.accountId
+      accountId: participantIdentity.player.accountId,
     }
   }
   for(const participant of detailsDoc.participants){
+    //TODO: Look up champion ID and give champion name and PNG link instead
     participants[participant.participantId].championId = participant.championId;
-    participantStatHistory[participant.participantId] = [];
+    participants[participant.participantId].teamId = participant.teamId;
+    //TODO: Look up spell IDs and give spell name and PNG link instead
+    participants[participant.participantId].spell1Id = participant.spell1Id;
+    participants[participant.participantId].spell2Id = participant.spell2Id;
+    participants[participant.participantId].statHistory = [];
   }
+
+  let timelineDoc = await timelineByGameId(gameId, region);
   //Breakdown frames
   const killEvents = [];
   for (const frame of timelineDoc.frames){
     for (const [id, pFrame] of Object.entries(frame.participantFrames)){
-      participantStatHistory[id].push({
+      participants[id].statHistory.push({
         totalGold: pFrame.totalGold,
         currentGold: pFrame.currentGold,
         level: pFrame.level,
@@ -40,8 +47,11 @@ const breakdown = async (req, res) => {
   //Get all of the key data out of timeline and details
   let game = {
     participants,
-    participantStatHistory,
-    killEvents
+    killEvents,
+    ticks: timelineDoc.frames.length,
+    timestamps: timelineDoc.frames.map(frame => {
+      return frame.timestamp
+    })
   };
   return res.status(200).json(game);
 };
